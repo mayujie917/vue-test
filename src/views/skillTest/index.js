6e73d81d545ae8914ef13973801db212a1a81968
mayujie917 <1009239228@qq.com>
2023-10-19 23:20:50 +0800

    信息提示

--- src/views/skillTest/index.js
+++ src/views/skillTest/index.js
@@ -1,62 +1,85 @@
 import { List } from "@/data/index.js";
 
 export let data = {
-	name: "express",
-	data() {
-		return {
-			currentIndex: 1, //当前大题题号
-			subIndex: 0, //当前答题下的题号
-			currentData: {
-				selectList: [],
-			}, //当前问题
-			list: [], //原始数据
-			selectedData: [], // 图文多选，已选中的数据
-			previewModal:false, // 大图预览
-      previewObj:{}, // 当前预览图片
-		};
-	},
+  name: "express",
+  data() {
+    return {
+      tipsModal: true, //提示弹窗
+      tipsTitle: "322333", // 提示内容
+      previewModal: false, // 大图预览
+      previewObj: {}, // 当前预览图片
+      currentIndex: 1, //当前大题题号
+      subIndex: 0, //当前答题下的题号
+      currentData: {
+        selectList: [],
+      }, //当前问题
+      list: [], //原始数据
+      selectedData: [], // 当前已选中的数据，每次切换题，从原数据更具checked 重新获取
+    };
+  },
 
-	mounted() {
-		this.list = List;
-		// 初始 取每个大题里面的第一个小题
-		this.currentData = this.list[this.currentIndex].children[0];
-	},
+  mounted() {
+    this.list = List;
+    // 初始 取每个大题里面的第一个小题
+    // this.currentData = this.list[this.currentIndex].children[0];
+    this.$set(
+      this,
+      "currentData",
+      this.list[this.currentIndex].children[this.subIndex]
+    );
+  },
 
-	methods: {
-		/**
-		 * 1.单选
-		 * @param {*} index
-		 */
-		clickSelectItem(item, index) {},
-		/**
-		 * 带有图片的多选
-		 * @param {*} e
-		 */
-		selectItemHandle(item) {
-			item.isChecked = !item.isChecked;
-			if (item.isChecked) {
-				this.selectedData.push(item);
-			} else {
-				item.isChecked = false;
-				let _i = this.selectedData.findIndex((e) => e.id == item.id);
-				console.log("index", _i);
-				this.selectedData.splice(_i, 1);
-			}
-		},
+  methods: {
+    /**
+     * 1.单选
+     * @param {*} index
+     */
+    clickSelectItem(item, index) {},
+    /**
+     * 带有图片的多选
+     * @param {*} e
+     */
+    selectItemHandle(item) {
+      item.isChecked = !item.isChecked;
+      if (item.isChecked) {
+        this.selectedData.push(item);
+      } else {
+        item.isChecked = false;
+        let _i = this.selectedData.findIndex((e) => e.id == item.id);
+        console.log("index", _i);
+        this.selectedData.splice(_i, 1);
+      }
+    },
     /**
      * 2大题
      */
     // 预览大图
-    previewImgHandle(item){
+    previewImgHandle(item, index) {
+      // 判断是否已经选过
+      if (item.isChecked) {
+        this.tipsTitle = "该物品已经选择过了！";
+        this.tipsModal = true;
+        return false;
+      }
       this.previewObj = item;
-      this.previewModal = true; 
+      this.previewModal = true;
     },
-    // 选中图片
-    selectImgHandle(){
+    // 确认选中图片
+    selectImgHandle() {
+      this.previewModal = false;
+      this.previewObj.isChecked = true;
+      this.selectedData.push(this.previewObj);
+    },
+    cancelPrevView() {
+      this.previewObj.isChecked = false;
       this.previewModal = false;
     },
 
-		preBtn() {},
-		nextBtn() {},
-	},
+    preBtn() {
+      this.selectedData = [];
+    },
+    nextBtn() {
+      this.selectedData = [];
+    },
+  },
 };
--- src/views/skillTest/index.vue
+++ src/views/skillTest/index.vue
@@ -25,24 +25,24 @@
         </div>
         <!-- 多张图片，选出某几张 -->
         <div class="qs-select-wrapper" v-if="currentData.type == 3">
-            <div class="qs-img-box">
-              <div class="qs-img-list" v-for="item in currentData.images" @click="previewImgHandle(item)">
-                <img :src="item.url"  />
-              </div>
-            </div>
-            <div class="qs-select">
-              <div class="qs-select-area">
-                <div class="qs-select-area-item">
-                  <p class="select-area-item" v-for="item in selectedData" :key="index">{{ item.label }}</p>
-                </div>
-              </div>
-              <div class="qs-select-list">
-                <p class="list-item" v-for="(item, index) in currentData.selectList"
-                  :class="item.isChecked ? 'list-item-active' : ''" :key="index" @click="selectItemHandle(item)">{{
-                    item.label }}</p>
-              </div>
+          <div class="qs-img-box">
+            <div class="qs-img-list" v-for="item in currentData.images" @click="previewImgHandle(item, index)">
+              <img :src="item.url" />
             </div>
           </div>
+          <div class="qs-select">
+            <div class="qs-select-area">
+              <div class="qs-select-area-item">
+                <p class="select-area-item" v-for="item in selectedData" :key="index">{{ item.label }}</p>
+              </div>
+            </div>
+            <div class="qs-select-list">
+              <p class="list-item" v-for="(item, index) in currentData.selectList"
+                :class="item.isChecked ? 'list-item-active' : ''" :key="index" @click="selectItemHandle(item)">{{
+                  item.label }}</p>
+            </div>
+          </div>
+        </div>
         <!-- 选择题类型 -->
         <div class="qs-select-wrapper" v-if="currentData.type == 2">
           <div class="qs-select-radio-list">
@@ -56,7 +56,7 @@
           </div>
         </div>
         <div class="qs-btns">
-          <el-button  size="small" @click="preBtn">上一题</el-button>
+          <el-button size="small" @click="preBtn">上一题</el-button>
           <el-button type="" size="small" @click="nextBtn">下一题</el-button>
         </div>
       </div>
@@ -67,8 +67,15 @@
         <img :src="previewObj.url" alt="">
       </div>
       <div class="pre-btn">
-        <el-button type="" size="small" @click="()=> previewModal=false">取消</el-button>
+        <el-button type="" size="small" @click="cancelPrevView">取消</el-button>
         <el-button type="primary" size="small" @click="selectImgHandle">确定</el-button>
+      </div>
+    </div>
+    <!-- 提示 -->
+    <div class="tips" v-show="tipsModal">
+      <div class="tips-box">
+        <p class="tips-title">{{ tipsTitle }}</p>
+        <el-button type="primary">确定</el-button>
       </div>
     </div>
   </div>
@@ -80,5 +87,4 @@ export default data;
 </script>
 <style lang="less" scoped>
 @import url('./index.less');
-
 </style>
